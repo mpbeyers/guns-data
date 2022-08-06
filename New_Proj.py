ffde9b388eb845a01000c85bd796a271bf98e0d1
Michael Beyers <mpbeyers@me.com>
2022-08-06 11:33:41 -0700

    File for New Project

---
+++ New_Proj.py
@@ -0,0 +1,21 @@
+My new file!
+for i in 'hello':
+print ('hello')
+#This is an alteration to the earlier file
+np.random.seed(42)
+x = np.random.randn(100)
+
+bins = np.linspace(-5, 5, 20)
+counts = np.zeros_like(bins)
+
+i = np.searchsorted(bins, x)
+
+np.add.at(counts, i, 1)
+
+#plot results
+plt.plot(bins, counts, linestyle='steps');
+
+##making another note  Will try to push...
+#Making another change for saving on a new branch
+#one more update to add to Updates folder
+#adding more stuff here too!
