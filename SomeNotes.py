51621dcb087ca0b970b9ff7fb2a46e8d384d3430
Michael Beyers <mpbeyers@me.com>
2022-08-08 08:28:59 -0700

    new add

--- SomeNotes.py
+++ SomeNotes.py
@@ -1,1 +1,4 @@
-#This will be a new file for updated branch
+##This will be a new file for updated branch
+#Why is this grayed? Can't hardly read...
+# Saving this for commit
+# OK Trying this one more time
